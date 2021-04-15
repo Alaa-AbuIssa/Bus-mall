977aaab280dc59de7602d4897d9149cbe11a38d6
Alaa-AbuIssa <alaaabuissa95@gmail.com>
2021-04-15 06:04:17 +0300

    documant format added

--- js/app.js
+++ js/app.js
@@ -2,18 +2,16 @@
 
 //get the images element by id
 let leftImageElement = document.getElementById('left-Image');
-let midImageElement = document.getElementById ('mid-Image');
+let midImageElement = document.getElementById('mid-Image');
 let rightImageElement = document.getElementById('right-Image');
-
 
 //define the global variable
 let userTriesCounter = 0;
-let maxTries =25;
+let maxTries = 25;
 let allProducts = [];
-let namesArr=[];
-let votesArr=[];
-let shownArr=[];
-
+let namesArr = [];
+let votesArr = [];
+let shownArr = [];
 
 // creat construction function for the products
 function Product(name, source) {
@@ -25,9 +23,6 @@ function Product(name, source) {
   namesArr.push(this.name);
   allProducts.push(this);
 }
-
-
-
 
 // creat the instensess of the product
 new Product('bag', 'images/bag.jpg');
@@ -53,56 +48,53 @@ new Product('wine-glass', 'images/wine-glass.jpg');
 
 // console.log(allProducts)
 
-
-
 // creat the function for generate random Number
 function generateRandomIndex() {
   return Math.floor(Math.random() * allProducts.length);
 }
 generateRandomIndex();
 
-
 //define the images index
 let leftImageIndex;
 let midImageIndex;
 let rightImageIndex;
 
-
-// creat the function just to check that the three imgs not equal and render them 
+// creat the function just to check that the three imgs not equal and render them
 function renderThreeProducts() {
+  leftImageIndex = generateRandomIndex();
+  midImageIndex = generateRandomIndex();
+  rightImageIndex = generateRandomIndex();
 
-  leftImageIndex=generateRandomIndex();
-  midImageIndex= generateRandomIndex();
-  rightImageIndex=generateRandomIndex();
-  
-  // define the shown product in an array 
-  let shownProductsArray=[];
-
+  // define the shown product in an array
+  let shownProductsArray = [];
 
   // check that the three imgs not equal
-  while (leftImageIndex===rightImageIndex ||leftImageIndex===midImageIndex || midImageIndex === rightImageIndex || shownProductsArray.includes(leftImageIndex || shownProductsArray.includes(rightImageIndex))) {
-    leftImageIndex=generateRandomIndex();
-    rightImageIndex=generateRandomIndex();
-    midImageIndex=generateRandomIndex();
+  while (
+    leftImageIndex === rightImageIndex ||
+    leftImageIndex === midImageIndex ||
+    midImageIndex === rightImageIndex ||
+    shownProductsArray.includes(
+      leftImageIndex || shownProductsArray.includes(rightImageIndex)
+    )
+  ) {
+    leftImageIndex = generateRandomIndex();
+    rightImageIndex = generateRandomIndex();
+    midImageIndex = generateRandomIndex();
   }
   // console.log(allProducts[leftImageIndex].name);
   // console.log (allProducts[rightImageIndex].source);
 
-
- //declar the images index into shown array
-  shownProductsArray=[leftImageIndex,rightImageIndex,midImageIndex];
-
+  //declar the images index into shown array
+  shownProductsArray = [leftImageIndex, rightImageIndex, midImageIndex];
 
   leftImageElement.src = allProducts[leftImageIndex].source;
   allProducts[leftImageIndex].shows++;
 
   midImageElement.src = allProducts[midImageIndex].source;
   allProducts[midImageIndex].shows++;
-  
-  rightImageElement.src =allProducts[rightImageIndex].source;
+
+  rightImageElement.src = allProducts[rightImageIndex].source;
   allProducts[rightImageIndex].shows++;
-
-
 }
 
 renderThreeProducts();
@@ -112,103 +104,79 @@ let divElement = document.getElementById('all-Images');
 //  adding event listener for divelement
 divElement.addEventListener('click', handleClick);
 
-
-
-
-// if the attempts is lower than the max tries and function to handle it 
+// if the attempts is lower than the max tries and function to handle it
 function handleClick(event) {
-
-  
-  if (userTriesCounter <=maxTries) {
-
+  if (userTriesCounter <= maxTries) {
     if (event.target.id === 'left-Image') {
       allProducts[leftImageIndex].votes++;
       renderThreeProducts();
       userTriesCounter++;
-
-
     } else if (event.target.id === 'right-Image') {
       allProducts[rightImageIndex].votes++;
       renderThreeProducts();
       userTriesCounter++;
-
-    } else if (event.target.id === 'mid-Image'){
+    } else if (event.target.id === 'mid-Image') {
       allProducts[midImageIndex].votes++;
       renderThreeProducts();
       userTriesCounter++;
-
+    } else {
+      alert('please click just on the pic');
+      userTriesCounter--;
     }
-    else {
-        alert ('please click just on the pic');
-        userTriesCounter--;
-    }
+  } else {
+    let button = document.getElementById('button');
 
-} else {
-     
-  let button=document.getElementById('button');
-  
-  divElement.removeEventListener('click',handleClick);
-  
-  //creat the button and add an event for it click 
-  let list=document.getElementById('productResults');
-  
-  list.appendChild(button);
-  
+    divElement.removeEventListener('click', handleClick);
 
-  button.hidden=false;  ;  
-  
-  
+    //creat the button and add an event for it click
+    let list = document.getElementById('productResults');
+
+    list.appendChild(button);
+
+    button.hidden = false;
+
     for (let i = 0; i < allProducts.length; i++) {
       votesArr.push(allProducts[i].votes);
       shownArr.push(allProducts[i].shows);
-      
     }
     updateStorage();
 
-    button.addEventListener('click',showingList);
-    button.addEventListener('click',chartgenerator);
-
+    button.addEventListener('click', showingList);
+    button.addEventListener('click', chartgenerator);
   }
 
-
-// Function to storage  products data to the local storage
-function updateStorage() {
-  let productStringArray = JSON.stringify(allProducts);
-  // console.log(stringVotesArray);
-  localStorage.setItem('Products', productStringArray);
+  // Function to storage  products data to the local storage
+  function updateStorage() {
+    let productStringArray = JSON.stringify(allProducts);
+    // console.log(stringVotesArray);
+    localStorage.setItem('Products', productStringArray);
   }
 }
 
-
-
-
-// function for update products before any votes 
-// with some friends helping 
+// function for update products before any votes
+// with some friends helping
 function getProductsData() {
   let productString = localStorage.getItem('Products');
 
   let productArray = JSON.parse(productString);
 
   if (productArray !== null) {
-      allProducts = productArray;
+    allProducts = productArray;
   }
 }
 
+function showingList() {
+  let productResult;
 
-    function showingList() {
-      
-      let productResult;
-    
-      for (let i = 0; i < Product.allProducts.length; i++) {
-        productResult=document.createElement('li');
-        list.appendChild(productResult);
-    
-        productResult.textContent=`${Product.allProducts[i].name} has ${Product.allProducts[i].votes} votes and was shown ${Product.allProducts[i].shows}`
-    }
-      button.removeEventListener('click',showingList);
-      
-    }
-  
+  for (let i = 0; i < Product.allProducts.length; i++) {
+    productResult = document.createElement('li');
+    list.appendChild(productResult);
+
+    productResult.textContent = `${Product.allProducts[i].name} has ${Product.allProducts[i].votes} votes and was shown ${Product.allProducts[i].shows}`;
+  }
+  button.removeEventListener('click', showingList);
+}
+
 // Chart function
 function chartgenerator() {
   var ctx = document.getElementById('myChart').getContext('2d');
@@ -222,25 +190,28 @@ function chartgenerator() {
           data: votesArr,
           backgroundColor: 'red',
           borderColor: 'orange',
-          borderWidth: 2
+          borderWidth: 2,
         },
         {
           label: '# of Shown',
           data: shownArr,
           backgroundColor: 'black',
           borderColor: 'orange',
-          borderWidth: 2
-        }]
+          borderWidth: 2,
+        },
+      ],
     },
     options: {
       scales: {
-        yAxes: [{
-          ticks: {
-            beginAtZero: true
-          }
-        }]
-      }
-    }
+        yAxes: [
+          {
+            ticks: {
+              beginAtZero: true,
+            },
+          },
+        ],
+      },
+    },
   });
 }
 getProductsData();
